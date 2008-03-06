2bd23a563dfe2d6a798dc7e72b5891a64f13d385
t.e.iversen <t.e.iversen@67f6b85d-ef44-0410-b042-8953758272c8>
2008-03-06 15:56:21 +0000

    chg adminMain.aspx/.cs

--- trunk/pro2final/Web/adminMain.aspx.cs
+++ trunk/pro2final/Web/adminMain.aspx.cs
@@ -9,14 +9,79 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 
+using myApp.IBLL;
+using myApp.Model;
+using System.Collections.Generic;
+
 public partial class adminMain : System.Web.UI.Page
 {
+    IProduktkategoriBLL pkBLL = BLLLoader.GetProduktkategoriBLL();
+
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!User.IsInRole("admin"))
         {
             Response.Redirect("~/Default.aspx");
         }
+        getKategorier();
     }
 
+    public List<Produktkategori> testkilde()
+    {
+        return pkBLL.getProduktkategorier();
+    }
+    private void getKategorier()
+    {
+        KatListe.Items.Clear();
+        try
+        {
+            List<Produktkategori> kategoriliste = pkBLL.getProduktkategorier();
+
+            if (kategoriliste.Count > 0)
+            {   
+                foreach (Produktkategori pk in kategoriliste)
+                    KatListe.Items.Add(new ListItem(pk.Navn, pk.ProduktkategoriID.ToString()));
+            }
+            else
+                KatListe.Items.Add("ingen kategorier er opprettet.");
+        }
+        catch (Exception ex)
+        {
+            Trace.Warn("DB'en tryna - kategori.ascx.cs");
+            Trace.Warn(ex.Message);
+        }
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Produktkategori pkat = new Produktkategori();
+        string s = KatNavn.Text.ToString();
+        if (KatListe.Items.FindByText(s) == null)
+        {
+
+
+            pkat.Navn = s;
+
+            if (!pkat.Navn.Equals(""))
+                pkBLL.nyProduktkategori(pkat);
+
+            KatNavn.Text = "";
+            getKategorier();
+        }
+        else RequiredFieldValidator1.IsValid = false;
+    }
+
+    protected void SlettKat_Click(object sender, EventArgs e)
+    {
+
+        
+        Response.Write(sender.ToString() +" "+ e.ToString());
+    }
+    protected void KatListe_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        Response.Write(sender.ToString() + " " + e.ToString());
+
+ 
+    }
 }
