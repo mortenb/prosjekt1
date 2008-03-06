f4a49b8913f60e38c6c5dd8914eb1c0a6887ff6c
mbyhring <mbyhring@67f6b85d-ef44-0410-b042-8953758272c8>
2008-03-06 09:04:51 +0000

    Lagt til logiske metoder til Handlevogn. LeggTilVareIHandlevogn, slettVare, slettHandlevogn

--- trunk/pro2final/Model/Handlevogn.cs
+++ trunk/pro2final/Model/Handlevogn.cs
@@ -7,14 +7,37 @@ namespace myApp.Model
 {
     public class Handlevogn 
     {
-        public Handlevogn() { }
-
         private List<Ordrelinje> _handleliste;
+
+        public Handlevogn() { _handleliste = new List<Ordrelinje>(); }
+
+       
 
         public List<Ordrelinje> Handleliste
         {
             get { return _handleliste; }
             set { this._handleliste = value; }
+        }
+
+        public void slettHandleliste()
+        {
+            _handleliste = null;
+        }
+
+        public void leggTilVareIHandlevogn(Ordrelinje ol)
+        {
+            _handleliste.Add(ol);
+        }
+
+        public void slettVareFraHandlevogn(Ordrelinje ol)
+        {
+            foreach (Ordrelinje ordre in _handleliste)
+            {
+                if (ordre.ProduktID == ol.ProduktID)
+                {
+                    _handleliste.Remove(ordre);
+                }
+            }
         }
     }
 } 
