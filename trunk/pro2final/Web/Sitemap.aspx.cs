a25a16cc606607a4af590edc020ab3968bbfa934
mbyhring <mbyhring@67f6b85d-ef44-0410-b042-8953758272c8>
2008-03-07 12:59:16 +0000

    Lagt til enkel navigasjon i form av breadcrumbs og sitemap

---
+++ trunk/pro2final/Web/Sitemap.aspx.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class Sitemap : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.TreeView1.DataSource = Page.Master.FindControl("SiteMapDataSource1");
+        this.TreeView1.DataBind();
+    }
+}
