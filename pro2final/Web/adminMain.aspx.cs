dde839675e9a21a73cab1ca89fc23e9ef175ca16
t.e.iversen <t.e.iversen@67f6b85d-ef44-0410-b042-8953758272c8>
2008-03-11 09:37:21 +0000

    Rydding i adminMain.aspx.cs

--- pro2final/Web/adminMain.aspx.cs
+++ pro2final/Web/adminMain.aspx.cs
@@ -15,8 +15,6 @@ using System.Collections.Generic;
 
 public partial class adminMain : System.Web.UI.Page
 {
-    //IProduktkategoriBLL pkBLL = BLLLoader.GetProduktkategoriBLL();
-    IProduktBLL pBLL = BLLLoader.GetProduktBLL();
     INyhetBLL nBLL = BLLLoader.GetNyhetBLL();
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -35,81 +33,6 @@ public partial class adminMain : System.Web.UI.Page
         }    
     }
 
-    private void getProdukter(int pkID)
-    {
-        List<Produkt> produktliste = pBLL.getProdukter(pkID);
-
-        if (produktliste.Count > 0)
-        {
-            
-        }
-        else
-        { 
-        }
-    }
-
- /*   private void getKategorier()
-    {
-        KatListe.Items.Clear();
-        try
-        {
-            List<Produktkategori> kategoriliste = pkBLL.getProduktkategorier();
-
-            if (kategoriliste.Count > 0)
-            {
-                KatListe.DataSource = kategoriliste;
-                KatListe.DataBind();
-                KatListe2.DataSource = kategoriliste;
-                KatListe2.DataBind();
-            }
-            else
-                KatListe.Items.Add("ingen kategorier er opprettet.");
-        }
-        catch (Exception ex)
-        {
-            Trace.Warn("DB'en tryna - adminMain.ascx.cs");
-            Trace.Warn(ex.Message);
-        }
-
-    }
-
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        Produktkategori pkat = new Produktkategori();
-        string s = KatNavn.Text.ToString();
-        if (KatListe.Items.FindByText(s) == null)
-        {
-
-
-            pkat.Navn = s;
-
-            if (!pkat.Navn.Equals(""))
-                pkBLL.nyProduktkategori(pkat);
-
-            KatNavn.Text = "";
-            getKategorier();
-        }
-        else RequiredFieldValidator1.IsValid = false;
-    }
-
-    protected void KatListe_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        Response.Write(sender.ToString() + " " + e.ToString());
-        KatNavn.Text = KatListe.SelectedItem.Text;
- 
-    }
-    protected void KatListe2_SelectedIndexChanged(object sender, EventArgs e)
-    {
-
-    }
-    protected void Slett_Click(object sender, EventArgs e)
-    {
-
-        //this.KatNavn.Text = this.KatListe.SelectedItem.Text;
-        Response.Write(this.KatListe.SelectedValue.ToString());
-
-    }
-  * */
     protected void Oppgavevelger_SelectedIndexChanged(object sender, EventArgs e)
     {
         Oppgaveview.ActiveViewIndex = Convert.ToInt32(Oppgavevelger.SelectedValue);
