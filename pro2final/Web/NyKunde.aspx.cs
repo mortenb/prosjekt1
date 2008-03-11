b19d8deda7ff7bf53d031f3f43d43d50cfc39a1b
t.e.iversen <t.e.iversen@67f6b85d-ef44-0410-b042-8953758272c8>
2008-03-11 11:10:05 +0000

    \Web\NyKunde.aspx.cs  \Web\Web.config Enabled smtp med bekreftelsesmail til kunde ved registrering

--- pro2final/Web/NyKunde.aspx.cs
+++ pro2final/Web/NyKunde.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using System.Net.Mail;
 
 public partial class NyKunde : System.Web.UI.Page
 {
@@ -20,5 +21,17 @@ public partial class NyKunde : System.Web.UI.Page
     {
         string kunde = CreateUserWizard1.UserName.ToString();
         Roles.AddUserToRole(kunde, "kunde");
+        MailAddress from = new MailAddress("webpro2gr1@gmail.com");
+        MailAddress recipient = new MailAddress("webpro2gr1@gmail.com");
+        MailMessage m = new MailMessage(from, recipient);
+        m.Body = "Din konto er registrert!\n\nBrukernavn:"+
+            kunde +"\nPassord: " +CreateUserWizard1.Password +
+            "\n\nRegistrert e-postadresse: " +CreateUserWizard1.Email +
+            "\n\nMed hilsen Webshopteamet.";
+        m.Subject = "Registreringsbekreftelse";
+ 
+        SmtpClient smc = new SmtpClient();
+        smc.EnableSsl= true;
+        smc.Send(m);
     }
 }
